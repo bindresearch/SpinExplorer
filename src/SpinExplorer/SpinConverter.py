0fe0dd5340add080e8e07066432a7679ae7ce030
jimmy95987 <james.eaton01@icloud.com>
2025-06-20 22:10:25 +0100

    Update SpinConverter.py

--- src/SpinExplorer/SpinConverter.py
+++ src/SpinExplorer/SpinConverter.py
@@ -2,7 +2,7 @@
 
 """MIT License
 
-Copyright (c) 2025 James Eaton
+Copyright (c) 2025 James Eaton, Andrew Baldwin (University of Oxford)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
