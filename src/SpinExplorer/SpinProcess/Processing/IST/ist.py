992c1c381779f731cf669e17cdfb68208ef19740
Gogs Karunanithy <gogs@bindresearch.org>
2026-07-21 10:00:09 +0100

    imrpovements to IST implementation

--- src/SpinExplorer/SpinProcess/Processing/IST/ist.py
+++ src/SpinExplorer/SpinProcess/Processing/IST/ist.py
@@ -1,7 +1,7 @@
 import numpy as np
 import nmrglue as ng
 from numpy.typing import NDArray
-from typing import Union
+from typing import Union, Optional
 import pyfftw
 import pyfftw.interfaces.numpy_fft as fft
 from SpinExplorer.SpinProcess.Processing.IST.sampling_utils import apply_sampling_schedule_nd, apply_sampling_schedule_to_2D_signal
@@ -13,33 +13,13 @@ pyfftw.interfaces.cache.set_keepalive_time(30)  # keep plans cached for 30s
 
 
 def write_as_nmrpipe(array: NDArray, spec_dic: dict, outfile: str):
-    #print(array.shape)
     ng.pipe.write(outfile, spec_dic, array, overwrite=True)
-
-# def get_thresh_signal_3d(signal_real: NDArray, signal_imaginary: NDArray, threshold: float) -> tuple[NDArray, NDArray]:
-    
-#     max_val = np.max(signal_real)
-#     signal_cutoff = threshold * max_val
-    
-#     # Compute soft threshold on real part
-#     thresh_real = signal_real - signal_cutoff
-#     thresh_real[thresh_real < 0.0] = 0.0
-    
-#     # Build a scaling mask from real part: where real was below cutoff, scale is 0;
-#     # elsewhere, scale is (thresholded / original) to apply proportional attenuation
-#     scale = np.where(signal_real > signal_cutoff, thresh_real / signal_real, 0.0)
-    
-#     # Apply same scaling to imaginary part
-#     thresh_imag = signal_imaginary * scale
-    
-#     return thresh_real, thresh_imag
-
-
 
 
 def get_thresh_signal_3d(signal_real: NDArray,
                         signal_imag: NDArray,
-                        threshold: float):
+                        threshold: float,
+                        window_dim2: NDArray,window_dim3:NDArray):
 
     def soft_thresh(x, threshold):
         mag = np.abs(x)
@@ -50,38 +30,28 @@ def get_thresh_signal_3d(signal_real: NDArray,
     thresh_real = soft_thresh(signal_real, threshold)
     thresh_imag = soft_thresh(signal_imag, threshold)
 
+    thresh_fid = retrieve_signal_ist_3d(thresh_real, thresh_imag, window_dim2, window_dim3)
+
+    thresh_real, thresh_imag = pack_signal_ist_3d(thresh_fid)
+
+
     return thresh_real, thresh_imag
 
-"""
-# protect the original functions that are working
+
 def ist_iteration_3d(data: NDArray,
                     threshold: float,
-                    sampling_schedule: Union[list[int], NDArray]) -> tuple[NDArray, NDArray, NDArray, np.floating]:
+                    sampling_schedule: Union[list[int], NDArray],
+                    window_dim2:NDArray,
+                    window_dim3:NDArray) -> tuple[NDArray, NDArray, NDArray, np.floating]:
 
     # FT along faster indirect (axis=1): collapse cos/sin pairs → complex, FT, take real
 
-    data = data[:, 0::2] + 1.0j * data[:, 1::2]       
-    #data = fft.fftshift(fft.fft(data, axis=-1), axes=-1)
-    data = ng.proc_base.fft_positive(data)
-    data_real = np.real(data)                                 
-    data_imag = np.imag(data)
-    
-    data_real = np.transpose(data_real)
-    data_imag = np.transpose(data_imag)
+    data_real, data_imag = pack_signal_ist_3d(data)
 
-    data_real = data_real[:,0::2] + 1.0j*data_real[:,1::2]
-    data_imag = data_imag[:,0::2] + 1.0j*data_imag[:,1::2]
-    
-
-    data_real = ng.proc_base.fft_positive(data_real)
-    data_imag = ng.proc_base.fft_positive(data_imag)
-    
-    thresh_sig_real, thresh_sig_imag = get_thresh_signal_3d(data_real, data_imag, threshold)
+    thresh_sig_real, thresh_sig_imag = get_thresh_signal_3d(data_real, data_imag, threshold, window_dim2, window_dim3)
     leftover_sig_real = data_real - thresh_sig_real
     leftover_sig_imag = data_imag - thresh_sig_imag
     l2_norm = np.linalg.norm(leftover_sig_real)
-    thresh_to_write = retrieve_signal_ist_3d(thresh_sig_real, thresh_sig_imag)
-
 
     # Inverse: re-expand both axes back to interleaved form
     leftover_sig = retrieve_signal_ist_3d(leftover_sig_real, leftover_sig_imag)  # (n3*2, n2*2)
@@ -91,42 +61,10 @@ def ist_iteration_3d(data: NDArray,
     return leftover_sig, thresh_sig_real, thresh_sig_imag, l2_norm
 
 
-def retrieve_signal_ist_3d(leftover_sig_real, leftover_sig_imag):
-    
-    leftover_sig_real = ng.proc_base.ifft_positive(leftover_sig_real)
-    leftover_sig_imag = ng.proc_base.ifft_positive(leftover_sig_imag)
-    
-
-    def unpack_complex(x):
-        out = np.zeros(x.shape[:-1] + (x.shape[-1]*2,), dtype=np.float32)
-        out[..., 0::2] = np.real(x)
-        out[..., 1::2] = np.imag(x)
-        return out
-
-    leftover_sig_real = unpack_complex(leftover_sig_real)
-    leftover_sig_imag = unpack_complex(leftover_sig_imag)
-
-    leftover_sig_real = np.transpose(leftover_sig_real)
-    leftover_sig_imag = np.transpose(leftover_sig_imag)
-
-    data = leftover_sig_real + 1j*leftover_sig_imag
-
-
-    data = ng.proc_base.ifft_positive(data)
-    data = unpack_complex(data)
-
-    return data
-"""
-
-def ist_iteration_3d(data: NDArray,
-                    threshold: float,
-                    sampling_schedule: Union[list[int], NDArray]) -> tuple[NDArray, NDArray, NDArray, np.floating]:
-
-    # FT along faster indirect (axis=1): collapse cos/sin pairs → complex, FT, take real
+def pack_signal_ist_3d(data: NDArray):
 
     data = data[:, 0::2] + 1.0j * data[:, 1::2]       
-    data = fft.fftshift(fft.fft(data, axis=-1), axes=-1)
-    #data = ng.proc_base.fft_positive(data)
+    data = fft.fft(data, axis=-1)
     data_real = np.real(data)                                 
     data_imag = np.imag(data)
     
@@ -136,44 +74,27 @@ def ist_iteration_3d(data: NDArray,
     data_real = data_real[:,0::2] + 1.0j*data_real[:,1::2]
     data_imag = data_imag[:,0::2] + 1.0j*data_imag[:,1::2]
     
-
-    # data_real = ng.proc_base.fft_positive(data_real)
-    # data_imag = ng.proc_base.fft_positive(data_imag)
-
-    # data_real = fft.fftshift(fft.fft(data_real, axis=-1), axes=-1)
-    # data_imag = fft.fftshift(fft.fft(data_imag, axis=-1), axes=-1)
         
     stacked = np.stack([data_real, data_imag])  # (2, ...)
-    stacked = fft.fftshift(fft.fft(stacked, axis=-1), axes=-1)
+    stacked = fft.fft(stacked, axis=-1)
     data_real, data_imag = stacked[0], stacked[1]
+    return data_real, data_imag
 
-    thresh_sig_real, thresh_sig_imag = get_thresh_signal_3d(data_real, data_imag, threshold)
-    leftover_sig_real = data_real - thresh_sig_real
-    leftover_sig_imag = data_imag - thresh_sig_imag
-    l2_norm = np.linalg.norm(leftover_sig_real)
+def retrieve_signal_ist_3d(leftover_sig_real: NDArray, leftover_sig_imag: NDArray, window_dim2: Optional[NDArray] = None, window_dim3: Optional[NDArray] = None):
 
-
-    # Inverse: re-expand both axes back to interleaved form
-    leftover_sig = retrieve_signal_ist_3d(leftover_sig_real, leftover_sig_imag)  # (n3*2, n2*2)
     
-    leftover_sig = apply_sampling_schedule_nd(leftover_sig, sampling_schedule, (1, 0))
-  
-    return leftover_sig, thresh_sig_real, thresh_sig_imag, l2_norm
-
-
-def retrieve_signal_ist_3d(leftover_sig_real, leftover_sig_imag):
-    
-    # leftover_sig_real = ng.proc_base.ifft_positive(leftover_sig_real)
-    # leftover_sig_imag = ng.proc_base.ifft_positive(leftover_sig_imag)
-
-    #leftover_sig_real = fft.ifft(fft.ifftshift(leftover_sig_real, axes = -1), axis=-1)
-    #leftover_sig_imag = fft.ifft(fft.ifftshift(leftover_sig_imag, axes = -1), axis=-1)
     
     stacked = np.stack([leftover_sig_real, leftover_sig_imag])
-    stacked = fft.ifft(fft.ifftshift(stacked, axes=-1), axis=-1)
+    stacked = fft.ifft(stacked, axis=-1)
     leftover_sig_real, leftover_sig_imag = stacked[0], stacked[1]
+    
+    if window_dim2 is None:
+        window_dim2 = np.ones(leftover_sig_real.shape[-1])
 
-    def unpack_complex(x):
+    leftover_sig_real*=window_dim2
+    leftover_sig_imag*=window_dim2
+
+    def unpack_complex(x: NDArray):
         out = np.empty(x.shape[:-1] + (x.shape[-1]*2,), dtype=np.float32)
         out[..., 0::2] = x.real
         out[..., 1::2] = x.imag
@@ -183,24 +104,29 @@ def retrieve_signal_ist_3d(leftover_sig_real, leftover_sig_imag):
     leftover_sig_imag = unpack_complex(leftover_sig_imag)
 
     
-    #leftover_sig_real = np.transpose(leftover_sig_real)
-    #leftover_sig_imag = np.transpose(leftover_sig_imag)
-    
     leftover_sig_real = np.ascontiguousarray(leftover_sig_real.T)
     leftover_sig_imag = np.ascontiguousarray(leftover_sig_imag.T)
 
     data = leftover_sig_real + 1j*leftover_sig_imag
 
+    data = fft.ifft(data, axis = -1)
 
-    # data = ng.proc_base.ifft_positive(data)
-    data = fft.ifft(fft.ifftshift(data, axes = -1), axis=-1)
+    if window_dim3 is None:
+        window_dim3 = np.ones(data.shape[-1])
+
+    data *= window_dim3
     data = unpack_complex(data)
+    
 
     return data
 
 
 def ist_3d(input_spec: NDArray,
            sampling_schedule: Union[list[int], NDArray],
+           max_time_dim2: float,
+           max_time_dim3: float,
+           r2_dim2: Optional[float]= None,
+           r2_dim3: Optional[float]= None,
            threshold: float = 0.9,
            terminate: float = 1e-4,
            convergence_tol: float = 1e-8,
@@ -216,6 +142,19 @@ def ist_3d(input_spec: NDArray,
         sampling_schedule = np.asarray(sampling_schedule)
         sampling_schedule = sampling_schedule[:, ::-1]
 
+    if r2_dim2 is None:
+        r2_dim2 = 1.5/(max_time_dim2)
+    
+    if r2_dim3 is None:
+        r2_dim3 = 1.5/(max_time_dim3)
+
+    window_vals_dim3 = np.arange(input_spec.shape[-1]/2)/input_spec.shape[-1]/2
+    window_dim3 = np.exp(-window_vals_dim3*r2_dim3*max_time_dim3)
+
+
+    window_vals_dim2 = np.arange(input_spec.shape[1]/2)/input_spec.shape[1]/2
+    window_dim2 = np.exp(-window_vals_dim2*r2_dim2*max_time_dim2)
+
     def _reconstruct_until_convergence(nus_fid: NDArray) -> tuple[NDArray,NDArray]:
         reconstructed_r = None
         
@@ -224,7 +163,7 @@ def ist_3d(input_spec: NDArray,
 
 
         for iteration in range(1, max_iter + 1):
-            nus_fid, threshold_sig_real, threshold_sig_imag, _ = ist_iteration_3d(nus_fid, threshold, sampling_schedule)
+            nus_fid, threshold_sig_real, threshold_sig_imag, _ = ist_iteration_3d(nus_fid, threshold, sampling_schedule, window_dim2, window_dim3)
             if reconstructed_r is None:
                 reconstructed_r = np.zeros_like(threshold_sig_real)
                 reconstructed_i = np.zeros_like(threshold_sig_imag)
@@ -280,27 +219,14 @@ def ist_3d(input_spec: NDArray,
     for i, result in results:
         recon_spec[i] = result
 
-    # for i in range(input_spec.shape[0]):
-    # #for i in range(1):
-    #     print(f"IST slice {i + 1} / {input_spec.shape[0]}")
-    #     slice_data = input_spec[i].copy()
-    #     recon_real, recon_imag = reconstruct(slice_data)
-    #     recon_slice = retrieve_signal_ist_3d(recon_real, recon_imag)
-    #     recon_spec[i] = recon_slice
+
     return recon_spec
 
 
-# def get_thresh_signal(signal_ft: NDArray, threshold: float)->NDArray:
-    
-#     max = np.max(signal_ft)
-#     signal_cutoff = threshold*max
-#     thresh_sig = signal_ft - signal_cutoff
-#     thresh_sig[thresh_sig<0.0] = 0.0
-#     return thresh_sig
-
 
 def get_thresh_signal(signal_ft: NDArray,
-                        threshold: float):
+                        threshold: float,
+                        window: NDArray):
 
     def soft_thresh(x, threshold):
         mag = np.abs(x)
@@ -308,36 +234,30 @@ def get_thresh_signal(signal_ft: NDArray,
         scale = np.maximum(0.0, mag - cutoff) / (mag + 1e-12)
         return x * scale
 
-
     thresh_real = soft_thresh(signal_ft, threshold)
+    thresh_fid = fft.ifft(thresh_real)
+    thresh_fid*=window
+
+    thresh_real = fft.fft(thresh_fid)
 
     return thresh_real
 
 
 def ist_iteration_2d(nus_fid:NDArray, 
                   threshold:float, 
-                  sampling_schedule:Union[list[int],NDArray])->tuple[NDArray,NDArray,np.floating]:
+                  sampling_schedule:Union[list[int],NDArray],
+                  window: NDArray)->tuple[NDArray,NDArray,np.floating]:
+    
     # consider whether we want to put zero-filling into this
     
-    """
-    # this is the original function
-    signal_ft = np.fft.fft(nus_fid)
-    threshold_sig = get_thresh_signal(signal_ft, threshold)
-    leftover_sig = signal_ft-threshold_sig
-    l2_norm = np.linalg.norm(leftover_sig)
-    leftover_fid = np.fft.ifft(leftover_sig)
-    leftover_fid = apply_sampling_schedule_nd(leftover_fid, sampling_schedule, (0,))
-    """
 
-    # with faster fft 
+
     signal_ft = fft.fft(nus_fid)
-    #signal_ft[...,0] = signal_ft[...,0]/0.5 
-    threshold_sig = get_thresh_signal(signal_ft, threshold)
+    threshold_sig = get_thresh_signal(signal_ft, threshold, window)
     leftover_sig = signal_ft-threshold_sig
     l2_norm = l2_norm = np.sqrt(np.vdot(leftover_sig, leftover_sig).real)
 
     leftover_fid = fft.ifft(leftover_sig)
-    #leftover_fid[...,0] = leftover_fid[...,0]*2.0
     leftover_fid = apply_sampling_schedule_nd(leftover_fid, sampling_schedule, (0,))
 
     return leftover_fid, threshold_sig, l2_norm
@@ -345,9 +265,11 @@ def ist_iteration_2d(nus_fid:NDArray,
 
 def ist_2d(input_spec: NDArray,
            sampling_schedule: Union[list[int], NDArray],
+           max_time: float,
+           r2: Optional[float] = None,
            threshold: float = 0.9,
            terminate: float = 0.001,
-           convergence_tol: float = 1e-10,
+           convergence_tol: float = 1e-6,
            max_iter: int = 4000,
            mode: int = 1,
            verb: bool = False) -> NDArray:
@@ -367,14 +289,20 @@ def ist_2d(input_spec: NDArray,
     mode            : 1 = converge on relative change, 2 = converge on L2 norm
     """
 
+    if r2 is None:
+        r2 = 1.2/(max_time)+1.0
+
+    window_vals = np.arange(input_spec.shape[-1])/input_spec.shape[-1]
+    window = np.exp(-window_vals*r2*max_time)
+
+
     def _reconstruct_until_convergence(nus_fid: NDArray) -> NDArray:
         reconstructed = np.zeros_like(nus_fid)
         prev_reconstructed = np.zeros_like(nus_fid)
         prev_norm = 0.0
 
-        nus_fid[...,0] = nus_fid[...,0]/2.0
         for iteration in range(1, max_iter + 1):
-            nus_fid, threshold_signal, _ = ist_iteration_2d(nus_fid, threshold, sampling_schedule)
+            nus_fid, threshold_signal, _ = ist_iteration_2d(nus_fid, threshold, sampling_schedule, window)
             reconstructed += threshold_signal
 
             curr_norm = np.sqrt(np.vdot(reconstructed, reconstructed).real)
@@ -397,7 +325,7 @@ def ist_2d(input_spec: NDArray,
         reconstructed = np.zeros_like(nus_fid)
 
         for iteration in range(1, max_iter + 1):
-            nus_fid, threshold_signal, l2_norm = ist_iteration_2d(nus_fid, threshold, sampling_schedule)
+            nus_fid, threshold_signal, l2_norm = ist_iteration_2d(nus_fid, threshold, sampling_schedule, window)
             reconstructed += threshold_signal
 
             if l2_norm <= terminate:
@@ -414,6 +342,7 @@ def ist_2d(input_spec: NDArray,
         reconstructed_slice = reconstruct(input_spec[i].copy())
         return i, reconstructed_slice
 
+
     recon_spec = np.zeros_like(input_spec)
     reconstruct = _reconstruct_until_convergence if mode == 1 else _reconstruct_until_l2
     recon_buffer = np.zeros_like(input_spec)
@@ -427,6 +356,5 @@ def ist_2d(input_spec: NDArray,
         recon_buffer[i] = result
 
     recon_spec = fft.ifft(recon_buffer, axis = -1)
-    recon_spec[...,0] = recon_spec[...,0]*2.0
 
     return recon_spec
